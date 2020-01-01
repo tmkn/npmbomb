21707d94d35b7401306aacf8657cc15df2def6a5
tmkn <tuomasklein@gmail.com>
2020-01-01 13:04:20 +0100

    asljf

--- tools/npmdata/npmdata.ts
+++ tools/npmdata/npmdata.ts
@@ -1,3 +1,26 @@
+import * as path from "path";
+import * as fs from "fs";
+import * as readline from "readline";
+
 import { Extractor } from "@tmkn/packageanalyzer";
 
-console.log(`Hello World`);
+const [,, foo] = process.argv;
+
+console.log(foo);
+
+(async () => {
+    const inputData = path.join(__dirname, `packages.txt`);
+    const outputDir = path.join(__dirname, `data`);
+    const rl = readline.createInterface({
+        input: fs.createReadStream(inputData)
+    });
+    let i = 1;
+
+    createOutDir(outputDir);
+
+    await Extractor.Extract(inputData, foo, outputDir, pa => ({name: pa.name}));
+})();
+
+function createOutDir(outDir: string): void {
+    fs.mkdirSync(outDir, { recursive: true });
+}
