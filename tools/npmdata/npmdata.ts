448f15b5ada5d830fb1e98c909977ee259cbcba8
tmkn <tuomasklein@gmail.com>
2020-07-12 01:18:58 +0200

    use webpack to bundle the extractor code

--- tools/npmdata/npmdata.ts
+++ tools/npmdata/npmdata.ts
@@ -3,17 +3,17 @@ import * as fs from "fs";
 
 import { Extractor } from "../../packageanalyzer/src/extractor";
 
-const [, , foo] = process.argv;
+const [, , npmDump] = process.argv;
 
-console.log(foo);
+console.log(npmDump);
 
 (async () => {
-    const inputData = path.join(__dirname, `..`, `..`, `..`, `packages.txt`);
-    const outputDir = path.join(__dirname, `..`, `..`, `..`, `data`);
+    const inputData = path.join(__dirname, `..`, `packages.txt`);
+    const outputDir = path.join(__dirname, `..`, `data`);
 
     createOutDir(outputDir);
 
-    await Extractor.Extract(inputData, foo, outputDir, pa => ({
+    await Extractor.Extract(inputData, npmDump, outputDir, pa => ({
         name: pa.name,
         version: pa.version,
         directDependencies: pa.directDependencyCount,
---
+++ tools/npmdata/webpack.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+    entry: {
+        npmdata: './tools/npmdata/npmdata.ts'
+    },
+    mode: 'production',
+    target: 'node',
+    node: {
+        __dirname: false,
+        __filename: false,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: [/node_modules/],
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+    ],
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, 'build'),
+    }
+};
