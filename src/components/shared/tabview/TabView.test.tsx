dcd1df04ddb357a2027d7868407b090825e9c26d
tmkn <tuomasklein@gmail.com>
2022-02-01 18:35:34 +0100

    upgraded copy-webpack-plugin

--- src/components/shared/tabview/TabView.test.tsx
+++ src/components/shared/tabview/TabView.test.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { render, fireEvent, waitForElement } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 
 import { TabView, ITab } from "./TabView";
 
